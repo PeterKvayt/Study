4498a745de654af8ce69583faa63454336aef083
PeterKvayt <peter.kvayt@gmail.com>
2019-10-12 22:15:59 +0300

    добавил ClientTCP и TCPListener

---
+++ NewClientTCP/NewClientTCP/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewClientTCP
+{
+    class Program
+    {
+        // Создаем клиента, используя конструктор по умолчанию 
+        
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient tcpClient = new TcpClient();
+                // Подключаемся к серверу 
+                tcpClient.Connect("127.0.0.1", 5001);
+                // Создаем поток, соединенный с сервером 
+                NetworkStream stream = tcpClient.GetStream();
+                // Формируем сообщение. Преобразуем его в массив байтов
+                while (true)
+                {
+                    Console.WriteLine("Write your message");
+                    string message = Console.ReadLine();
+                    byte[] data = Encoding.UTF8.GetBytes(message);
+                    // Отправка сообщения 
+                    stream.Write(data, 0, data.Length);
+                    if (message == "exit")
+                    {
+                        break;
+                    }
+                    byte[] response = new byte[256];
+                    int responseLength = stream.Read(response, 0, response.Length);
+                    Console.WriteLine(Encoding.UTF8.GetString(response, 0, response.Length));
+                }
+                // Закрываем потоки 
+                stream.Close();
+                tcpClient.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
+
---
+++ NewServerTCP/NewServerTCP/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewServerTCP
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                TcpListener server = new TcpListener(localAddr, 5001);
+                // Запускаем сервер 
+                server.Start();
+                Console.WriteLine("Ожидание подключений... ");
+                // Получаем входящее подключение 
+                TcpClient client = server.AcceptTcpClient();
+                // Получаем сетевой поток для чтения и записи 
+                NetworkStream stream = client.GetStream();
+                while (true)
+                {
+                    byte[] data = new byte[256];
+                    int bytes = stream.Read(data, 0, data.Length); // Читаем сообщение
+                    string message = Encoding.UTF8.GetString(data, 0, bytes);
+                    if (message != "exit")
+                    {
+                        double result = 0;
+                        string[] args = message.Split(new char[] { ' ' });
+                        foreach (var item in args)
+                        {
+                            try
+                            {
+                                result += Convert.ToDouble(item);
+                            }
+                            catch (Exception)
+                            {
+                                throw;
+                            }
+                        }
+                        // Отображаем сообщение 
+                        byte[] response = Encoding.UTF8.GetBytes("Result : " + result.ToString());
+
+                        stream.Write(response, 0, response.Length);
+                    }
+                    else
+                    {
+                        stream.Close();
+                        client.Close();
+                        // Закрываем слушающий объект 
+                        server.Stop();
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
